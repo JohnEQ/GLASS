48d2454f9782501986104673251bf084d66fb422
Anthony Flamenco <anthony.flamenco.csc@gmail.com>
2016-08-22 15:24:00 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es" class="no-js">
+    <head>
+        <meta charset="utf-8">
+		<title>BANDESAL</title>
+	</head>
+	<body align="center">
+		<div align="center">
+			<h1>Portal de Transparencia</h1>
+		</div>
+	</body>
+</html>
